36585ef71c25a7e591e0207b2d124d0e1298569f
SMY <yu.seungmok1111@gmail.com>
2025-07-22 22:43:20 -0500

    asdf

--- src/components/InfoBoard.jsx
+++ src/components/InfoBoard.jsx
@@ -1,25 +1,27 @@
 import "./InfoBoard.css";
+import { Link } from "react-router-dom";
 import testImg from '../assets/gameboardclose.png'
 
 // TODO: test if varying lengths affect the layout.
-export default function InfoBoard( props ) {
+export default function InfoBoard({ event_title, event_description_brief, event_thumbnail_src, event_page_address }) {
+
     function InfoSingle() {
-        return(
-            <div className="infosingle">
-                <div className="infosingle-content">
-                    <img src={testImg}/>
-                    <h3>Test meeting</h3>
-                    <p>This is a description of a very fun event or some
-                        very serious, strategic meeting.</p>
+        return (
+            <Link to={event_page_address}>
+                <div className="infosingle">
+                    <div className="infosingle-content">
+                        <img src={event_thumbnail_src} alt={event_title} />
+                        <h3>{event_title}</h3>
+                        <p>{event_description_brief}</p>
+                    </div>
                 </div>
-            </div>
+            </Link>
         );
     }
- // TODO: Props should also include ROUTE address
+
+    // TODO: Props should also include ROUTE address
     return (
         <div className="infoboard">
-            <InfoSingle />
-            <InfoSingle />
             <InfoSingle />
             <InfoSingle />
         </div>
