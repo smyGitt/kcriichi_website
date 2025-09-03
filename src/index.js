fd881f2f42745d4fc7d3dd3f5ee0db32ea02ee04
MechaKorea1664 <kevin.yu1111@gmail.com>
2025-09-03 16:40:01 -0500

    remove BrowserRouter import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // NOT COMPATIBLE WITH GITHUB PAGES
+//import { BrowserRouter } from 'react-router-dom'; // NOT COMPATIBLE WITH GITHUB PAGES
 import { HashRouter } from 'react-router-dom'; // COMPATIBLE WITH GITHUB PAGES
 import './index.css';
 import App from './App';
